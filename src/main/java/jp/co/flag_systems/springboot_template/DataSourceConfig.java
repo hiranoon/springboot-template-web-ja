e46246642034f65de0a61b35404f61c756f05275
hiranoon <hirano.masashi@gmail.com>
2016-02-04 16:55:51 +0900

    Add heroku jdbc setting.

--- src/main/java/jp/co/flag_systems/springboot_template/DataSourceConfig.java
+++ src/main/java/jp/co/flag_systems/springboot_template/DataSourceConfig.java
@@ -2,7 +2,9 @@ package jp.co.flag_systems.springboot_template;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +16,10 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DataSourceConfig {
 
+    /** DataSourceに関するプロパティ設定. */
+    @Autowired
+    DataSourceProperties properties;
+
     /**
      * DataSource 設定.
      * @return {@link DataSource} オブジェクト
@@ -21,7 +27,25 @@ public class DataSourceConfig {
     @Bean
     @ConfigurationProperties("spring.datasource")
     public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
+        String url = System.getenv("JDBC_DATABASE_URL"); // Heroku で定義されています。
+        DataSourceBuilder factory;
+        if (url != null) {
+            // JDBC_DATABASE_URL の設定を利用します。
+            factory = DataSourceBuilder
+                    .create(this.properties.getClassLoader())
+                    .url(url);
+        } else {
+            // プロパティファイルを利用します。
+            factory = DataSourceBuilder
+                    .create(this.properties.getClassLoader())
+                    .url(this.properties.getUrl())
+                    .username(this.properties.getUsername())
+                    .password(this.properties.getPassword());
+        }
+        return factory.build();
+//
+//
+//        return DataSourceBuilder.create().build();
     }
 
 }
