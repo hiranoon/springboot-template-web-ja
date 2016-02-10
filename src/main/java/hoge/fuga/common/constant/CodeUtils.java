5accecf459866e61e21b22d697fa5451d6608e95
hiranoon <hirano.masashi@gmail.com>
2016-02-11 06:47:01 +0900

    Change CodeUtils#getCodeName method interface.

--- src/main/java/hoge/fuga/common/constant/CodeUtils.java
+++ src/main/java/hoge/fuga/common/constant/CodeUtils.java
@@ -18,25 +18,22 @@ public class CodeUtils {
     /**
      * 指定されたコードのクラスとコードIDからコード名称を取得します.
      * <p>
-     * 存在しないコードIDが指定された場合は空文字を返却します.
      * <blockquote><pre>
      * // 以下の例では "DF" が取得されます.
      * CodeUtil.getCodeName(CodeConst.PositionCode.class, "2");
      * </pre></blockquote>
      * </p>
      * <p>
-     * また、指定されるクラスは {@link Code} を実装した enum であること、
      * コードには同じコードIDが存在していないことを前提としています.
+     * 存在しないコードIDが指定された場合は空文字を返却します.
      * </p>
-     *
      * @param codeClass コードのクラス
      * @param id コードID
      * @return コード名称. 存在しないコードIDの場合は空文字.
      */
-    public static String getCodeName(Class<? extends Enum<?>> codeClass, String id) {
+    public static String getCodeName(Class<? extends Code> codeClass, String id) {
         // コードのクラスの中から id が合致する要素を抽出します.
         Optional<String> codeName = Arrays.stream(codeClass.getEnumConstants())
-                .map(x -> (Code)x)                 // Code が指定されることを前提としてキャストします.
                 .filter(x -> x.getId().equals(id)) // コードIDが合致する Code を抽出します.
                 .map(x -> x.getName())             // Code から Code の name に変換します.
                 .findFirst();                      // 最初の要素を返却していますが、仕様上一意に決まる想定です.
