db7d9b2d22aed67d4ef4ca1c3e6a6cfc8baefd68
hiranoon <hirano.masashi@gmail.com>
2016-02-14 16:57:40 +0900

    Add SecurityUtils.

---
+++ src/main/java/hoge/fuga/common/security/SecurityUtils.java
@@ -0,0 +1,39 @@
+package hoge.fuga.common.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+/**
+ * Spring Security に関連するユーティリティクラスです.
+ * @author hirano
+ */
+public class SecurityUtils {
+
+    /**
+     * 隠蔽されたコンストラクタ.
+     */
+    private SecurityUtils() {
+    }
+
+    /**
+     * 認証済みの {@link UserDetails} を取得します.
+     * <p>
+     * Controller クラスのメソッド引数に、下記のように定義して取得してください.
+     * <pre>
+     * String method(@AuthenticationPrincipal UserDetails userDetails ...) { ... }
+     * </pre>
+     * ただし、Service クラスなどでは、 Controller から順次引数で渡していく必要があります.
+     * これを避けるために当該メソッドを用意しています.
+     * </p>
+     * @return 認証済みの {@link UserDetails}. 認証されていない場合は null を返却します.
+     */
+    public static UserDetails getUserDetails() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // 認証されていない場合は null を返却します.
+        if (authentication == null) {
+            return null;
+        }
+        // UserDetails を返却します.
+        return (UserDetails) authentication.getPrincipal();
+    }
+}
